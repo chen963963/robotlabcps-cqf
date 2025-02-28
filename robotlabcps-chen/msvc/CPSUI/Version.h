41961dc4c04237937d5f1d9eb48e3c70ce9b1dd3
chen963963 <woaisanguozhi@163.com>
2025-02-28 11:09:15 +0800

    1

--- robotlabcps-chen/msvc/CPSUI/Version.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#define VERSION_MAJOR				1
-#define VERSION_MINOR				0
-#define VERSION_REVISION_NO			0
-#define VERSION_BUILD_NO			682
-
-#define VERSION_BUILD_TIME			"2024/03/01 23:48:02"
-
